a9c9e2c91d2d0cdfa67080c6358a932634004ed8
PeggyKimotho <peggykim20@gmail.com>
2026-06-02 13:07:43 +0300

    Edit Bg colour

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -43,7 +43,6 @@ function Navbar() {
     { name: 'About', href: '#about' },
     { name: 'Services', href: '#services' },
     { name: 'Projects', href: '#projects' },
-    { name: 'Testimonials', href: '#testimonials' },
     { name: 'Contact', href: '#contact' },
   ]
 
